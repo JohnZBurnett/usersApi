a3e98a6e8e5d30dc75431726c49427e008281a2b
JohnZBurnett <tuninator@gmail.com>
2018-10-28 10:54:28 -0400

    add code to map from JSON

--- src/main/java/com/restful/users/services/UserResource.java
+++ src/main/java/com/restful/users/services/UserResource.java
@@ -1,8 +1,13 @@
 package com.restful.users.services;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
@@ -24,7 +29,28 @@ public class UserResource {
 	}
 	
 	@POST
-	public Response createNewUser() {
+	public Response createNewUser(InputStream is) {
+		ObjectMapper mapper = new ObjectMapper(); 
+		try {
+			User user = mapper.readValue(is, User.class); 
+			System.out.println(user);
+			String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user); 
+			System.out.println(jsonString);
+		} catch (JsonParseException e) { e.printStackTrace();}
+		  catch (JsonMappingException e) {e.printStackTrace();}
+		  catch (IOException e) { e.printStackTrace(); }
+		
+		return Response.ok().build(); 
+	}
+	
+	@GET
+	@Path("{id}")
+	public Response getOneUser(@PathParam("id") int id) {
+		final User user = userDB.get(id); 
+		
+		if (user == null) {
+			throw new WebApplicationException(Response.Status.NOT_FOUND); 
+		}
 		return Response.ok().build(); 
 	}
 }
