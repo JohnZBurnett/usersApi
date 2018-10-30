e9c4548246e1a4c996b90b100899a90fa0e628f4
JohnZBurnett <tuninator@gmail.com>
2018-10-30 11:33:38 -0400

    remove DB methods from UserResource

--- src/main/java/com/restful/users/services/UserResource.java
+++ src/main/java/com/restful/users/services/UserResource.java
@@ -47,15 +47,6 @@ public class UserResource {
 		
 	}
 	
-	private static Connection getConnection() throws URISyntaxException, SQLException, NullPointerException {
-	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
-	    String username = dbUri.getUserInfo().split(":")[0];
-	    String password = dbUri.getUserInfo().split(":")[1];
-	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-	    return DriverManager.getConnection(dbUrl, username, password);
-	}
-	
 	@GET
 	public Response getAllUsers() throws JsonProcessingException, NullPointerException, SQLException, URISyntaxException {
 		ObjectMapper mapper = new ObjectMapper();
@@ -65,68 +56,11 @@ public class UserResource {
 		
 	}
 	
-	public HashMap getAllUsersFromDb() throws NullPointerException, SQLException, URISyntaxException {
-		HashMap<String, ArrayList<User>> resultsMap = new HashMap<String, ArrayList<User>>(); 
-		ArrayList userArray = new ArrayList<User>();  
-		PreparedStatement pst = databaseHelper.getConnection().prepareStatement(queryToGetAllUsers);
-		ResultSet queryResults = pst.executeQuery(); 
-		while (queryResults.next()) {
-			User resultAsUser = new User();
-		    resultAsUser.setId(queryResults.getInt("id"));
-		    resultAsUser.setFirstName(queryResults.getString("firstName"));
-		    resultAsUser.setLastName(queryResults.getString("lastName"));
-		    userArray.add(resultAsUser); 
-		}
-		
-		resultsMap.put("users", userArray); 
-		return resultsMap; 
-		
-	}
-	
-	public ResultSet getUserFromDb(int userId) {
-		ResultSet user = null; 
-		
-		try (Connection connection = getConnection()) {
-			PreparedStatement pst = connection.prepareStatement(queryToGetSpecificUser);
-			pst.setInt(1,  userId);
-			user = pst.executeQuery();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		
-		return user; 
-	}
-	public User addUserToDb(User user) {
-		try (Connection connection = getConnection()) {
-			PreparedStatement pst = connection.prepareStatement(queryToInsertUser);
-			pst.setString(1,  user.getFirstName());
-			pst.setString(2,  user.getLastName());
-			pst.execute();
-			ResultSet lastUpdatedUserResults = pst.getResultSet();
-			if(lastUpdatedUserResults.next()) {
-			   int lastUpdatedUserId = lastUpdatedUserResults.getInt(1);
-			   user.setId(lastUpdatedUserId);
-			}
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		
-		return user; 
-	}
-	
 	@POST
 	@Consumes("application/json")
 	public Response createNewUser(InputStream is) {
 		ObjectMapper mapper = new ObjectMapper(); 
-		try (Connection connection = getConnection()) {
+		try (Connection connection = databaseHelper.getConnection()) {
 			User user = mapper.readValue(is, User.class); 
 			user = databaseHelper.addUserToDb(user); 
 			String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user);
